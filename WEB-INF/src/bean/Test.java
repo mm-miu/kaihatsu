91694c2e4f5115234dc32e7c247a12ff363c5491
Tsukakou-0303 <kmm2559324@stu.o-hara.ac.jp>
2026-04-23 12:31:43 +0900

    Test.java

--- WEB-INF/src/bean/Test.java
+++ WEB-INF/src/bean/Test.java
@@ -6,55 +6,49 @@ public class Test implements java.io.Serializable {
     private Subject subject;     // 科目
     private School school;       // 学校
     private ClassNum classNum;   // クラス
-    private int num;             // 回数（NO）
+    private int no;             // 回数（NO）
     private int point;           // 得点（POINT）
 
     //ゲッター
     public Student getStudent() {
         return student;
     }
-
-    public Subject getSubject() {
-        return subject;
-    }
-
-    public School getSchool() {
-        return school;
+    public void setStudent(Student student) {
+        this.student = student;
     }
 
     public ClassNum getClassNum() {
         return classNum;
     }
+    public void setClassNum(ClassNum classNum) {
+        this.classNum = classNum;
+    }
 
-    public int getNum() {
-        return num;
+    public Subject getSubject() {
+        return subject;
+    }
+    public void setSubject(Subject subject) {
+        this.subject = subject;
+    }
+
+    public School getSchool() {
+        return school;
+    }
+    public void setSchool(School school) {
+        this.school = school;
+    }
+
+    public int getNo() {
+        return no;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
     }
 
     public int getPoint() {
         return point;
     }
-
-    //セッター
-    public void setStudent(Student student) {
-        this.student = student;
-    }
-
-    public void setSubject(Subject subject) {
-        this.subject = subject;
-    }
-
-    public void setSchool(School school) {
-        this.school = school;
-    }
-
-    public void setClassNum(ClassNum classNum) {
-        this.classNum = classNum;
-    }
-
-    public void setNum(int num) {
-        this.num = num;
-    }
-
     public void setPoint(int point) {
         this.point = point;
     }
