fdcadbb60d718777f41e7adfbb784e402376a5d8
Tsukakou-0303 <kmm2559324@stu.o-hara.ac.jp>
2026-04-24 10:06:04 +0900

    TestRegistAction.java

---
+++ WEB-INF/src/bean/TestListSubject.java
@@ -0,0 +1,51 @@
+package bean;
+
+import java.util.Map;
+
+public class TestListSubject {
+
+    private int entYear;
+    private String studentNo;
+    private String studentName;
+    private String classNum;
+    private Map<Integer,Integer> points;
+
+    public int getEntYear() {
+        return entYear;
+    }
+    public String getStudentNo() {
+        return studentNo;
+    }
+    public String getStudentName() {
+        return studentName;
+    }
+    public String getClassNum() {
+        return classNum;
+    }
+    public Map<Integer,Integer> getPoints() {
+        return points;
+    }
+    public String getPoint(key:int) {
+    //？
+    }
+
+
+    public void setEntYear(int entYear) {
+        this.entYear = entYear;
+    }
+    public void setStudentNo(String studentNo) {
+        this.studentNo = studentNo;
+    }
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+    public void setClassNum(String classNum) {
+        this.classNum = classNum;
+    }
+    public void setPoints(Map<Integer,Integer> points) {
+        this.points = points;
+    }
+    public void putPoint(int key,int value) {
+    //ここだけわからないので放置   
+    }
+}
