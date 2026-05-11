e3655e70ba869a9750b8b7bc1eb3b26c736e128e
UltM-Cin1000 <kmm2559340@stu.o-hara.ac.jp>
2026-05-11 12:59:41 +0900

    ユーザー管理機能

--- WEB-INF/src/dao/TeacherDao.java
+++ WEB-INF/src/dao/TeacherDao.java
@@ -3,6 +3,11 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import bean.School;
+
 import bean.Teacher;
 
 public class TeacherDao extends Dao {
@@ -84,6 +89,7 @@ public class TeacherDao extends Dao {
                 teacher.setId(rs.getString("id"));
                 teacher.setName(rs.getString("name"));
                 teacher.setSchool(schoolDao.get(rs.getString("school_cd")));
+                teacher.setAuthority(rs.getString("authority"));
             } else {
                 // 存在しない場合
                 // 先生インスタンスにnullをセット
@@ -111,4 +117,328 @@ public class TeacherDao extends Dao {
         }
         return teacher;
     }
+    //list(自身)
+    public List<Teacher>myfilter(School school,String id) throws Exception{
+        List<Teacher> list=new ArrayList<>();
+            // データベースへのコネクションを確立
+        Connection con=getConnection();
+        // プリペアードステートメント
+        PreparedStatement st=null;
+        try {
+            // プリペアードステートメントにSQL文をセット
+            st=con.prepareStatement(
+                "SELECT NAME,SCHOOL_CD,ID,PASSWORD FROM TEACHER WHERE SCHOOL_CD=? AND ID=?"
+            );
+            // プリペアードステートメントに学校コードをバインド
+            st.setString(1, school.getCd());
+            st.setString(2,id);
+            // プリペアードステートメントを実行
+            ResultSet rs=st.executeQuery();
+            
+
+            // リザルトセットを全件走査
+            while (rs.next()) {
+                // リストにクラス番号を追加
+                Teacher t=new Teacher();
+                School sch=new School();
+                sch.setName(rs.getString("school_cd"));
+                t.setSchool(sch);
+                t.setName(rs.getString("name"));
+                t.setId(rs.getString("id"));
+                t.setPassword(rs.getString("password"));
+                list.add(t);
+            }
+
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            // プリペアードステートメントを閉じる
+            if (st!=null) {
+                try {
+                    st.close();;
+                } catch (SQLException sqle) {
+                    throw sqle;
+                }
+            }
+            // コネクションを閉じる
+            if (con!=null) {
+                try {
+                    con.close();
+                } catch (SQLException sqle) {
+                    throw sqle;
+                }
+            }
+        }
+        return list;
+    }
+
+    //list(低権力)
+    public List<Teacher> rowfilter(School school) throws Exception {
+        // リストを初期化
+        List<Teacher> list=new ArrayList<>();
+            // データベースへのコネクションを確立
+        Connection con=getConnection();
+        // プリペアードステートメント
+        PreparedStatement st=null;
+        try {
+            // プリペアードステートメントにSQL文をセット
+            st=con.prepareStatement(
+                "SELECT NAME,SCHOOL_CD,ID,PASSWORD FROM TEACHER WHERE SCHOOL_CD=? order by AUTHORITY ,NAME"
+            );
+            // プリペアードステートメントに学校コードをバインド
+            st.setString(1, school.getCd());
+            // プリペアードステートメントを実行
+            ResultSet rs=st.executeQuery();
+            SchoolDao schoolDao=new SchoolDao();
+
+            // リザルトセットを全件走査
+            while (rs.next()) {
+                // リストにクラス番号を追加
+                Teacher t=new Teacher();
+                t.setSchool(schoolDao.get(rs.getString("school_cd")));
+                t.setName(rs.getString("name"));
+                
+                list.add(t);
+            }
+
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            // プリペアードステートメントを閉じる
+            if (st!=null) {
+                try {
+                    st.close();;
+                } catch (SQLException sqle) {
+                    throw sqle;
+                }
+            }
+            // コネクションを閉じる
+            if (con!=null) {
+                try {
+                    con.close();
+                } catch (SQLException sqle) {
+                    throw sqle;
+                }
+            }
+        }
+        return list;
+    }
+    //List(中権力)
+    public List<Teacher> Mdfilter(School school) throws Exception {
+        // リストを初期化
+        List<Teacher> list=new ArrayList<>();
+            // データベースへのコネクションを確立
+        Connection con=getConnection();
+        // プリペアードステートメント
+        PreparedStatement st=null;
+        try {
+            // プリペアードステートメントにSQL文をセット
+            st=con.prepareStatement(
+                "SELECT NAME,SCHOOL_CD,ID,PASSWORD FROM TEACHER Where school_cd=?");
+            // プリペアードステートメントに学校コードをバインド
+            st.setString(1, school.getCd());
+            // プリペアードステートメントを実行
+            ResultSet rs=st.executeQuery();
+                SchoolDao schoolDao=new SchoolDao();
+            // リザルトセットを全件走査
+            while (rs.next()) {
+                // リストにクラス番号を追加
+                Teacher t=new Teacher();
+                t.setSchool(schoolDao.get(rs.getString("school_cd")));
+                t.setName(rs.getString("name"));
+                t.setId(rs.getString("id"));
+                t.setPassword(rs.getString("password"));
+                list.add(t);
+            }
+
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            // プリペアードステートメントを閉じる
+            if (st!=null) {
+                try {
+                    st.close();;
+                } catch (SQLException sqle) {
+                    throw sqle;
+                }
+            }
+            // コネクションを閉じる
+            if (con!=null) {
+                try {
+                    con.close();
+                } catch (SQLException sqle) {
+                    throw sqle;
+                }
+            }
+        }
+        return list;
+    }
+
+    //List(高権力)
+    public List<Teacher> Hifilter() throws Exception {
+        // リストを初期化
+        List<Teacher> list=new ArrayList<>();
+            // データベースへのコネクションを確立
+        Connection con=getConnection();
+        // プリペアードステートメント
+        PreparedStatement st=null;
+        try {
+            // プリペアードステートメントにSQL文をセット
+            st=con.prepareStatement(
+                "SELECT NAME,SCHOOL_CD,ID,PASSWORD FROM TEACHER");
+            // プリペアードステートメントに学校コードをバインド
+            
+            // プリペアードステートメントを実行
+            ResultSet rs=st.executeQuery();
+                
+                
+            // リザルトセットを全件走査
+            while (rs.next()) {
+                // リストにクラス番号を追加
+                School sch=new School();
+                Teacher t=new Teacher();
+                sch.setName(rs.getString("school_cd"));
+                t.setSchool(sch);
+                t.setName(rs.getString("name"));
+                t.setId(rs.getString("id"));
+                t.setPassword(rs.getString("password"));
+                list.add(t);
+            }
+
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            // プリペアードステートメントを閉じる
+            if (st!=null) {
+                try {
+                    st.close();;
+                } catch (SQLException sqle) {
+                    throw sqle;
+                }
+            }
+            // コネクションを閉じる
+            if (con!=null) {
+                try {
+                    con.close();
+                } catch (SQLException sqle) {
+                    throw sqle;
+                }
+            }
+        }
+        return list;
+    }
+    // ユーザー登録
+    public boolean save(Teacher teacher) throws Exception {
+        // コネクションを確立
+        Connection con=getConnection();
+        // プリペアードステートメント
+        PreparedStatement st=null;
+        // 実行件数
+        int count=0;
+
+        try {
+            // データベースから学生を取得
+            Teacher old=get(teacher.getId());
+            if (old==null) {
+                // 学生が存在しなかった場合
+                // プリペアードステートメントにINSERT文をセット
+                st=con.prepareStatement(
+                    "insert into Teacher(id, password,name,school_cd,authority) values(?, 'password', ?, ?,'3')"
+                );
+
+                // プリペアードステートメントに値をバインド
+                st.setString(1, teacher.getId());
+                st.setString(2, teacher.getName());
+                st.setString(3, teacher.getSchool().getCd());
+                
+            } else {
+                // 学生が存在した場合
+                // プリペアードステートメントにUPDATE文をセット
+                st=con.prepareStatement(
+                    "update teacher set id=?, password=?, name=?, school_cd=? where no=?"
+                );
+                st.setString(1, teacher.getId());
+                st.setString(2, teacher.getPassword());
+                st.setString(3, teacher.getName());
+                st.setString(4, teacher.getSchool().getCd());
+               
+                
+            }
+            // プリペアードステートメントを実行
+            count=st.executeUpdate();
+
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            // プリペアードステートメントを閉じる
+            if (st!=null) {
+                try {
+                    st.close();
+                } catch (SQLException sqle) {
+                    throw sqle;
+                }
+            }
+            // コネクションを閉じる
+            if (con!=null) {
+                try {
+                    con.close();
+                } catch (SQLException sqle) {
+                    throw sqle;
+                }
+            }
+        }
+
+        if (count>0) {
+            // 実行件数が1件以上ある場合
+            return true;
+        } else {
+            // 実行件数が0件の場合
+            return false;
+        }
+    }
+    //重複確認
+    public boolean existsTeacherNo(String schoolCd, String id) throws Exception {
+        // コネクションを確立
+        Connection con=getConnection();
+        // プリペアードステートメント
+        PreparedStatement st=null;
+        // リザルトセット
+        ResultSet rs=null;
+        // SQL文の条件
+        // SQL文の在学フラグ条件
+        String condetionNo="and id=?";
+
+        try {
+            // プリペアードステートメントにSQL文をセット
+            st=con.prepareStatement("select * from teacher where school_cd=?"+condetionNo);
+            // プリペアードステートメントに学校コードをバインド
+            st.setString(1, schoolCd);
+            st.setString(2, id);
+            // プリペアードステートメントを実行
+            rs=st.executeQuery();
+
+            return rs.next();
+            
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            // プリペアードステートメントを閉じる
+            if (st!=null) {
+                try {
+                    st.close();
+                } catch (SQLException sqle) {
+                    throw sqle;
+                }
+            }
+            // コネクションを閉じる
+            if (con!=null) {
+                try {
+                    con.close();
+                } catch (SQLException sqle) {
+                    throw sqle;
+                }
+            }
+        }
+    }
+    
 }
---
+++ scoremanager/main/teacher_list.jsp
@@ -0,0 +1,120 @@
+<%-- 学生一覧JSP --%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8" %>
+<%@ taglib prefix="c" uri="jakarta.tags.core" %>
+
+<c:set var="content">
+    <section>
+        <h2 class="main-title">ユーザー管理</h2>
+        <c:choose>
+        <c:when test="${ authority=='1' || authority=='2'}">
+        <div class="new">
+            <a href="TeacherCreate.action">新規登録</a>
+        </div>
+        </c:when>
+        <c:otherwise></c:otherwise>
+        </c:choose>
+        <form method="get" class="form">
+            <div class="row">
+                <div class="col-4">
+                    <label for="student-f1-select">学校コード</label>
+                    <select  id="student-f1-select" name="f1" >
+                        <option value="0">--------</option>
+                        <option value="upcd" label="昇順">昇順</option>
+                        <option value="downcd" label="降順">降順</option>
+                    </select>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-4">
+                    <label for="student-f1-select">名前</label>
+                    <select  id="student-f1-select" name="f1" >
+                        <option value="0">--------</option>
+                        <option value="upname" label="昇順">昇順</option>
+                        <option value="downname" label="降順">降順</option>
+                    </select>
+                </div>
+            </div>
+            
+        </form>
+      
+        <c:choose>
+            <c:when test="${AllList.size()>0}">
+                <div>検索結果：${AllList.size()}件</div>
+                <table class="table">
+                    <tr>
+                        <th>学校コード</th>
+                       
+                        <th>氏名</th>
+                        <th>id</th>
+                        
+                    </tr>
+                    <tr>
+                        <c:forEach var="T_My" items="${MyList}">
+                    
+                        <td>${T_My.school.getName()}</td>  
+                        <td>${T_My.name}</td>
+                        <td>${T_My.id}</td>
+                        
+                            
+                        <td><a href="StudentUpdate.action?no=${T_My.id}">変更</a></td>
+                        
+                    </tr>
+                    </c:forEach>
+                </table>
+                <c:choose>
+                
+                <c:when test="${ authority=='1' || authority=='2'}">
+                <table class="table">
+                    <tr>
+                        <th>学校コード</th>
+                        
+                        <th>氏名</th>
+                        <th>id</th>
+                        
+                    </tr>
+                        <c:forEach var="T_all" items="${AllList}">
+                    <tr>
+                        <td>${T_all.school.getName()}</td>
+                        <td>${T_all.name}</td>
+                        <td>${T_all.id}</td>
+                        
+                            
+                        <td><a href="StudentUpdate.action?no=${T_all.id}">変更</a></td>
+                        
+                    </tr>
+                    </c:forEach>
+                </table>
+                </c:when>
+                <c:otherwise>
+                <table class="table">
+                    <tr>
+                        <th>学校コード</th>
+                        
+                        <th>氏名</th>
+                    </tr>
+                    <c:forEach var="T_ALL" items="${AllList}">
+                        <tr>
+                            <td>${T_ALL.school.getName()}</td>
+                            <td>${T_ALL.name}</td>
+                        </tr>
+                    </c:forEach>
+                </table>
+                </c:otherwise>
+                </c:choose>
+            </c:when>
+            <c:otherwise>
+                <div>ユーザー情報が存在しませんでした。</div>
+            </c:otherwise>
+        </c:choose>
+    </section>
+</c:set>
+
+<c:import url="/common/base.jsp">
+    <c:param name="title">
+        得点管理システム
+    </c:param>
+    <c:param name="scripts"></c:param>
+    <c:param name="content" value="${content}"></c:param>
+</c:import>
